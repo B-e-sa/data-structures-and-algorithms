6a9f6369959657ada12065f8a3b2817647c90ec8
B-e-sa <joaospereira963@gmail.com>
2024-07-18 14:32:52 -0300

    first commit

---
+++ data-structures/linked-list/singly.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+class SinglyListNode
+{
+public:
+    SinglyListNode(int value, SinglyListNode *next = nullptr)
+    {
+        this->next = next;
+        this->value = value;
+    }
+    int value;
+    SinglyListNode *next;
+};
+
+class SinglyLinkedList
+{
+public:
+    SinglyListNode *head = nullptr;
+    void add(SinglyListNode *node)
+    {
+        if (head == nullptr)
+        {
+            head = node;
+            return;
+        }
+
+        SinglyListNode *currentNode = head;
+        while (currentNode->next != nullptr)
+        {
+            currentNode = currentNode->next;
+        }
+        currentNode->next = node;
+    }
+};
+
+int main()
+{
+    SinglyLinkedList list;
+    SinglyListNode firstNode(5, nullptr);
+    SinglyListNode secondNode(13, nullptr);
+    SinglyListNode thirdNode(98, nullptr);
+    SinglyListNode fourthNode(71, nullptr);
+
+    list.add(&firstNode);
+    list.add(&secondNode);
+    list.add(&thirdNode);
+    list.add(&fourthNode);
+
+    SinglyListNode *iterator = list.head;
+    while (iterator != nullptr) {
+        cout << iterator->value;
+        iterator = iterator->next;
+    }
+
+    return 0;
+}
